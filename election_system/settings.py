5b1170716e3b7e4801dbe350160a46a18a6b1ad1
Emmanueljoe <emmanuelletsu18@gmail.com>
2024-12-03 19:27:47 +0000

    updated settings.py

--- election_system/settings.py
+++ election_system/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['presidentialrtc.site,145.223.85.248,www.presidentialrtc.site']
+ALLOWED_HOSTS = ['presidentialrtc.site','145.223.85.248','www.presidentialrtc.site']
 
 
 # Application definition
