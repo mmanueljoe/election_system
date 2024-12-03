7c15f87dd70fdeec33ffe6f79023223d3709158b
Emmanueljoe <emmanuelletsu18@gmail.com>
2024-12-03 18:39:48 +0000

    updated settings.py

--- election_system/settings.py
+++ election_system/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['presidentialrtc.site,145.223.85.248,www.presidentialrtc.site']
 
